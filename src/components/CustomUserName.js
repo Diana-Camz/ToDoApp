fcd05db30dec9b2d7db53e495e6b22daf20c898e
Diana Campos <dcampos0495@gmail.com>
2025-01-17 10:00:27 -0600

    improve CustomUserName to change information depending on the type of screen

--- src/components/CustomUserName.js
+++ src/components/CustomUserName.js
@@ -3,34 +3,32 @@ import React from 'react'
 import {customUserName} from '../styles/components/customUserName'
 import { containers } from '../styles/containers'
 
-const CustomUserName = ({name, image, home}) => {
-  if(home){
+const CustomUserName = ({name, image, screen}) => {
+
+     const userNameStyles = screen === 'home' 
+        ? [containers.userName_home, {marginLeft: 10}, customUserName.title_home] 
+        : [containers.userName,{marginRight: 10}, customUserName.title]
+
     return (
-        <View style={containers.userName_home}>
+        <View style={userNameStyles}>
             <View>
-                <Image source={image} style={customUserName.image}/>
+                { screen === 'home'
+                ? (<Image source={image} style={customUserName.image_home}/>)
+                : (<Image source={image} style={customUserName.image}/>)}
             </View>
-            <View style={customUserName.container_name}>
-                <Text style={customUserName.title}>{name}</Text>
-                <Text style={customUserName.welcome_description}>Hello! what will we do today?</Text>
+            <View style={userNameStyles[1]}>
+                <Text style={userNameStyles[2]}>{name}</Text>
+                { screen === 'home'
+                ? ( <Text style={customUserName.welcome_description}>Hello! what will we do today?</Text> )
+                : null }
             </View>
-            <Pressable onPress={() => console.log('logout button pressed')} style={customUserName.container_logout}>
-                <Text style={customUserName.logout_text}>Log out</Text>
-            </Pressable>
+            { screen === 'home' 
+                ? ( <Pressable onPress={() => console.log('logout button pressed')} style={customUserName.marginLeft_logout}>
+                        <Text style={customUserName.logout_text}>Log out</Text>
+                    </Pressable> )
+                : null }
         </View>
       )
-  } else {
-    return (
-        <View style={containers.userName}>
-            <View>
-                <Image source={image} style={customUserName.image}/>
-            </View>
-            <View style={customUserName.container_name}>
-                <Text style={customUserName.title}>{name}</Text>
-            </View>
-        </View>
-      )
-  }
 }
 
 export default CustomUserName
