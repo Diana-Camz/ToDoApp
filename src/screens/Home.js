c30933cc3f41f753e6f729b136c5f46d38dbadc8
Diana Campos <dcampos0495@gmail.com>
2025-01-22 12:57:06 -0600

    change some containers to corresponding file

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
 import React from 'react'
 import { default as category } from '../data/exampleCategory'
 import { default as tasks } from '../data/exampleTask'
@@ -15,7 +15,7 @@ const Home = ({navigation}) => {
     <View style={containers.main}>
       <CustomUserName screen={'home'} name={'Diana Campos'} image={require('../images/user-avatar.png')} />
       <View>
-        <View style={styles.container_categories_text}>
+        <View style={containers.homeSections}>
             <Text style={styles.categories_title}>CATEGORIES</Text>
             <Text style={styles.categories_option}>See more</Text>
         </View>
@@ -31,11 +31,11 @@ const Home = ({navigation}) => {
         </View>
       </View>
       <View>
-        <View style={styles.container_tasks_text}>
+        <View style={containers.homeSections}>
             <View>
                 <Text style={styles.tasks_title}>TODAY TASK</Text>
             </View>
-            <View style={styles.container_tasks_options}>
+            <View style={containers.homeSectionTask}>
                 <Text style={styles.tasks_option}>filters</Text>
                 <Text style={styles.tasks_option}>See all</Text>
             </View>
@@ -45,7 +45,7 @@ const Home = ({navigation}) => {
             data={tasks}
             keyExtractor={item => item.id.toString()}
             renderItem={({item}) => 
-                <Task title={item.title} emoji={item.emoji} time={item.time} status={item.status} priority={item.priority}/>
+                <Task {...item}/>
             }
             showsVerticalScrollIndicator={false}/>
         </View>
@@ -60,12 +60,6 @@ const Home = ({navigation}) => {
 export default Home
 
 const styles = StyleSheet.create({
-    container_categories_text: {
-        marginTop: 10,
-        paddingLeft: 30,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
     categories_title: {
         color:'#ffffff',
     },
@@ -73,19 +67,8 @@ const styles = StyleSheet.create({
         color:'#657FB1',
         marginRight: 40,
     },
-    container_tasks_text:{
-        marginTop: 10,
-        paddingLeft: 30,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
     tasks_title: {
         color:'#ffffff',
-    },
-    container_tasks_options:{
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end', 
     },
     tasks_option:{
         color:'#657FB1',
