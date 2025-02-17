a5fc9b1f8709b68edfa481f4a33e33c6dbfddca5
Diana Campos <dcampos0495@gmail.com>
2025-02-17 13:18:22 -0600

    feat: add database connection setup with mysql

--- server/database.js
+++ server/database.js
@@ -42,24 +42,75 @@ export async function getAllCategories(){
     return rows;
 };
 
-export async function getTasksWithCategories(){
+export async function getTasksWithCategories(user_id){
     const [rows] = await pool.query(
         `SELECT t.id AS task_id, t.title, t.status, t.date, t.time, t.priority, t.description, t.emoji, t.user_id,
         GROUP_CONCAT(c.name ORDER BY c.name SEPARATOR ' | ') AS categories
         FROM tasks t
         LEFT JOIN task_categories tc ON t.id = tc.task_id
-        LEFT JOIN categories c ON tc.category_id = c.id
-        GROUP BY t.id;`
+        LEFT JOIN categories c ON tc.category_id = c.id 
+        WHERE t.user_id = ?
+        GROUP BY t.id;`,
+        [user_id]
     );
-    console.log(rows);
+    return rows;
 };
 
 //CREATE
+export async function createTask(title, status, date, time, priority, description, emoji, user_id, categories) {
+    const [result] = await pool.query(
+        `INSERT INTO tasks (title, status, date, time, priority, description, emoji, user_id)
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, [title, status, date, time, priority, description, emoji, user_id]);
+         const taskId = result.insertId;
+         return getTaskByID(taskId);
+};
+
+export async function createTaskCategory(task_id, category_id) {
+    const [result] = await pool.query(
+        `INSERT INTO task_categories (task_id, category_id)
+         VALUES (?, ?)`, [task_id, category_id]); 
+}
+
 //UPDATE
+export async function updateTask(id, values) {
+    const fields = Object.keys(values).filter(field => field !== 'categories');
+    const updateValues = fields.map(field => `${field} = ?`).join(', ')
+    
+    if(fields.length === 0){
+        throw new Error("No valid fields provided for update.");
+    };
+
+    const updateParams = fields.map(field => values[field]);
+    updateParams.push(id);
+
+    const [result] = await pool.query(`UPDATE tasks SET ${updateValues} WHERE id = ?`,
+        updateParams
+    );
+
+    if(values.categories){
+        await updateTaskCategories(id, values.categories);
+    }
+    return result;
+};
+
+export async function updateTaskCategories(task_id, categories){
+    await pool.query(`DELETE FROM task_categories WHERE task_id = ?`,
+        [task_id]
+    );
+
+    if(categories.length > 0){
+        const placeholders = categories.map(() => '(?, ?)').join(', ');
+        const values = categories.flatMap(category_id => [task_id, category_id])
+        await pool.query(`INSERT INTO task_categories (task_id, category_id) VALUES ${placeholders}`, 
+        values
+    );
+    }
+}
+
 export async function toggleStatus(id, value){
     const newValue = value === true ? "TRUE" : "FALSE";
     const [result] = await pool.query(
-        `UPDATE tasks SET status = ${newValue} WHERE id = ?`
+        `UPDATE tasks SET status = ${newValue} WHERE id = ?`,
         [id]
     );
     return result;
@@ -67,8 +118,8 @@ export async function toggleStatus(id, value){
 //DELETE
 export async function deleteTask(id){
     const [result] = await pool.query(
-        `DELETE * FROM tasks WHERE id = ?`
+        `DELETE FROM tasks WHERE id = ?`,
         [id]
     );
     return result;
-}
+};
